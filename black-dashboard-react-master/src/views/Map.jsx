7a57d6eec56975610af69b5e650b9e17127df9f0
javierfuentesm <javier.fuentesm@hotmail.com>
2019-11-16 19:08:58 -0600

    En progreso renderizar la posicion inicial si aun no ingresa nada

--- black-dashboard-react-master/src/views/Map.jsx
+++ black-dashboard-react-master/src/views/Map.jsx
@@ -4,13 +4,33 @@ const {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
+  Marker,
   DirectionsRenderer
 } = require("react-google-maps");
 
 class Map extends React.Component {
   state = {
-    directions: null
+    directions: null,
+    currentLatLng: ""
   };
+
+  componentDidMount() {
+    const getGeoLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+          console.log(position.coords);
+          this.setState(prevState => ({
+            currentLatLng: {
+              ...prevState.currentLatLng,
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            }
+          }));
+        });
+      }
+    };
+    getGeoLocation();
+  }
 
   componentDidUpdate(prevProps) {
     const DirectionsService = new google.maps.DirectionsService();
@@ -42,11 +62,26 @@ class Map extends React.Component {
 
   render() {
     const GoogleMapRoute = withScriptjs(
-      withGoogleMap(props => (
-        <GoogleMap defaultCenter={this.props.addressIni} defaultZoom={13}>
-          <DirectionsRenderer directions={this.state.directions} />
-        </GoogleMap>
-      ))
+      withGoogleMap(props => {
+       // if ((this.props.addressIni&&this.props.addressFin)) {
+          return (
+            <GoogleMap defaultCenter={this.props.addressIni} defaultZoom={13}>
+              <DirectionsRenderer directions={this.state.directions} />
+            </GoogleMap>
+          );
+/*         } else {
+          return (
+            <GoogleMap
+              defaultCenter={this.state.currentLatLng}
+              defaultZoom={13}
+            >
+              <Marker position={this.state.currentLatLng} />
+
+              <DirectionsRenderer directions={this.state.directions} />
+            </GoogleMap>
+          );
+        } */
+      })
     );
 
     return (
@@ -59,21 +94,5 @@ class Map extends React.Component {
     );
   }
 }
-
-/* const Map = props => {
-
-  const MapLoader = withScriptjs(Ride);
-
-  return (
-    <>
-      <Ride addressIni={props.addressIni} addressFin={props.addressFin}></Ride>;
-
-      <MapLoader
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw&v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: "100%" }} />}
-      />
-    </>
-  );
-}; */
 
 export default Map;
