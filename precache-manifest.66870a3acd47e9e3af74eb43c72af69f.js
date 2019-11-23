746bfc197865a9cb9a38d800ae9aa812bbf5d035
rjfm <javier.fuentesm@hotmail.com>
2019-11-23 11:17:15 -0600

    Updates

---
+++ precache-manifest.66870a3acd47e9e3af74eb43c72af69f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "db8b093ffcc677599ef1",
+    "url": "/Movi/static/css/main.2a3268de.chunk.css"
+  },
+  {
+    "revision": "db8b093ffcc677599ef1",
+    "url": "/Movi/static/js/main.db8b093f.chunk.js"
+  },
+  {
+    "revision": "1f380492dcf88e782700",
+    "url": "/Movi/static/css/1.a3135113.chunk.css"
+  },
+  {
+    "revision": "1f380492dcf88e782700",
+    "url": "/Movi/static/js/1.1f380492.chunk.js"
+  },
+  {
+    "revision": "03582ad6d113a82b8584",
+    "url": "/Movi/static/js/runtime~main.03582ad6.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/Movi/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/Movi/static/media/react-logo.eb6be414.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/Movi/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/Movi/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/Movi/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/Movi/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "bfbd613a10f078c9249508bdda79ddb0",
+    "url": "/Movi/index.html"
+  }
+];
